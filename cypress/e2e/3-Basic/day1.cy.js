f3ef5192bfa6fafcd3d1d9ce3bb4ea42bdf40424
mahadevimurud020 <madu34690@gmail.com>
2025-04-21 07:11:36 -0500

    first commit

--- cypress/e2e/3-Basic/day1.cy.js
+++ cypress/e2e/3-Basic/day1.cy.js
@@ -1,1 +1,13 @@
 ///<reference types='cypress' />
+
+describe('verify login page',function(){
+    it('verify login page with valid data',function(){
+        cy.visit('https://webdriveruniversity.com/Contact-Us/contactus.html')
+        cy.get('[name="first_name"]').type('madu')
+        cy.get('[name="last_name"]').type('murud')
+        cy.get('[name="email"]').type('madu23@gmail.com')
+        cy.get('[name="message"]').type('I am Learning Cypress')
+        cy.get('[type="submit"]').click()
+        cy.get('h1').should('have.text','Thank You for your Message!')
+    })
+})
